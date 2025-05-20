490443fda556f605ac3646a00a261dde2ab25d90
lazywhizard17 <gokulakrishnad17@gmail.com>
2025-05-20 20:42:09 +0530

    Update transfer.py

--- transfer.py
+++ transfer.py
@@ -3,8 +3,8 @@ import subprocess
 cmd = [
     'scp',
     '-r',
-    r'C:/Users/rybac/Downloads/toTransfer', # Could be any custom directory. 
-    'gokula17@192.168.29.3:/home/gokula17/newAdditions/' # Could be any directory in ubuntu system. But it is just agreed upon to be newAdditions
+    r'C:/Users/rybac/Downloads/media/toTransfer', # Could be any custom directory. 
+    'gokula17@192.168.29.3:/home/gokula17/mediaServer/public/media/newAdditions/' # Could be any directory in ubuntu system. But it is just agreed upon to be newAdditions
 ]
 
 result = subprocess.run(cmd, capture_output= True, text= True)
